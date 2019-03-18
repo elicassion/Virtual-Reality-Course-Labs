3430031e6cc231f786a9949aad0fa22fea193340
elicassion <elicassion@sjtu.edu.cn>
2019-03-17 21:49:28 -0400

    [lab2] enlarging when gazing

---
+++ VRLab2/Assets/Scripts/Teleport.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public GameObject shoppingCart;
+
+    bool isInCart = false;
+    Vector3 initPosOnShelf;
+
+    public void ToggleCart()
+    {
+        if (isInCart)
+        {
+            removeCart();
+        } else
+        {
+            AddToCart();
+        }
+    }
+
+    public void removeCart()
+    {
+        transform.position = initPosOnShelf;
+        isInCart = false;
+    }
+
+    public void AddToCart()
+    {
+        transform.position = new Vector3(shoppingCart.transform.position.x,
+            shoppingCart.transform.position.y + 0.2f,
+            shoppingCart.transform.position.z);
+        isInCart = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+        initPosOnShelf = transform.position;
+        isInCart = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- VRLab2/Assets/Scripts/VRGaze.cs
+++ VRLab2/Assets/Scripts/VRGaze.cs
@@ -8,9 +8,15 @@ public class VRGaze : MonoBehaviour {
     public float totalTime = 2;
     bool gvrStatus;
     float gvrTimer;
+
+    public int distanceOfRay = 10;
+    private RaycastHit _hit;
+
+    GameObject curObj;
+
 	// Use this for initialization
 	void Start () {
-		
+        curObj = null;
 	}
 	
 	// Update is called once per frame
@@ -19,6 +25,23 @@ public class VRGaze : MonoBehaviour {
         {
             gvrTimer += Time.deltaTime;
             imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        
+        if (Physics.Raycast(ray, out _hit, distanceOfRay))
+        {
+            if (_hit.transform.CompareTag("Item"))
+            {
+                curObj = _hit.transform.gameObject;
+                curObj.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+                if (imgGaze.fillAmount == 1)
+                {
+                    curObj.transform.localScale = new Vector3(1, 1, 1);
+                    curObj.GetComponent<Teleport>().ToggleCart();
+                }
+            }
+            
         }
 	}
 
@@ -32,5 +55,7 @@ public class VRGaze : MonoBehaviour {
         gvrStatus = false;
         gvrTimer = 0;
         imgGaze.fillAmount = 0;
+        curObj.transform.localScale = new Vector3(1, 1, 1);
+        curObj = null;
     }
 }
