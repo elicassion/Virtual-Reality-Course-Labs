91f302cdc8fbb7fd0c873a0ff12da4361e5c2441
elicassion <elicassion@sjtu.edu.cn>
2019-03-26 22:40:43 -0400

    move

---
+++ VRLab2/Assets/testjoystick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testjoystick : MonoBehaviour
+{
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    void Update()
+    {
+        detectPressedKeyOrButton();
+    }
+    public void detectPressedKeyOrButton()
+    {
+        foreach (KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
+                Debug.Log("KeyCode down: " + kcode);
+        }
+    }
+}
