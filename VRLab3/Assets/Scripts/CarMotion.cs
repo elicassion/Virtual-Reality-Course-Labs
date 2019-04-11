1c3efbfc33529b45715e074ee0aa5bfb007b13cb
elicassion <elicassion@sjtu.edu.cn>
2019-04-10 20:52:03 -0400

    lab3

---
+++ VRLab3/Assets/Scripts/CarMotion.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMotion : MonoBehaviour {
+    enum Direction
+    {
+        Z_FORWARD, X_FORWARD, Z_BACKWARD, X_BACKWARD
+    }
+    private Direction carDirection = Direction.Z_FORWARD;
+
+    bool isBuildMode = false;
+
+    public GameObject UIController;
+
+    // Use this for initialization
+    void Start () {
+        transform.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (UIController.GetComponent<UIControl>().isBuildMode)
+        {
+
+        }
+        else
+        {
+            carMotion();
+        }
+	}
+
+    void carMotion()
+    {
+        float carSpeed = 0.02f;
+        switch (carDirection)
+        {
+            case Direction.Z_FORWARD:
+                if (transform.position.z >= 0.1f)
+                {
+                    turnYClockwise90(transform);
+                    carDirection = Direction.X_BACKWARD;
+                    break;
+                }
+                break;
+            case Direction.X_FORWARD:
+                if (transform.position.x >= 0.1f)
+                {
+                    turnYClockwise90(transform);
+                    carDirection = Direction.Z_FORWARD;
+                    break;
+                }
+                break;
+            case Direction.Z_BACKWARD:
+                if (transform.position.z <= -0.1f)
+                {
+                    turnYClockwise90(transform);
+                    carDirection = Direction.X_FORWARD;
+                    break;
+                }
+                break;
+            case Direction.X_BACKWARD:
+                if (transform.position.x <= -0.1f)
+                {
+                    turnYClockwise90(transform);
+                    carDirection = Direction.Z_BACKWARD;
+                    break;
+                }
+                break;
+        }
+        transform.Translate(Vector3.forward * carSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * carSpeed * Time.deltaTime);
+    }
+
+    void turnYClockwise90(Transform t)
+    {
+        t.Rotate(-Vector3.up * 90);
+    }
+}
---
+++ VRLab3/Assets/Scripts/UIControl.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+
+    public GameObject XZNav;
+    public GameObject XZRot;
+    public GameObject YNav;
+
+    public GameObject BuildModeBtn;
+    public GameObject CarBtn;
+    public GameObject HouseBtn;
+    public GameObject AirplaneBtn;
+    public GameObject HotairballoonBtn;
+    public GameObject SaveCurrentObjBtn;
+    public GameObject ExitBuildModeBtn;
+
+    public GameObject CarObj;
+    public GameObject HouseObj;
+    public GameObject AirplaneObj;
+    public GameObject HotairballoonObj;
+
+    public bool isBuildMode;
+
+    SimpleTouchController XZNavController;
+    SimpleTouchController XZRotController;
+    SimpleTouchController YNavController;
+
+    public bool isBuildingObj;
+    GameObject currentObj;
+    string curObjType;
+
+    float UIControlRate = 0.005f;
+
+    
+
+    
+
+	// Use this for initialization
+	void Start () {
+        //BuildModeBtn = GameObject.Find("BuildModeBtn");
+        //CarBtn = GameObject.Find("CarBtn");
+        //CarObj = GameObject.Find("CarObj");
+        CarBtn.SetActive(false);
+        HouseBtn.SetActive(false);
+        AirplaneBtn.SetActive(false);
+        HotairballoonBtn.SetActive(false);
+        ExitBuildModeBtn.SetActive(false);
+        SaveCurrentObjBtn.SetActive(false);
+        isBuildMode = false;
+        isBuildingObj = false;
+
+        XZNavController = XZNav.GetComponent<SimpleTouchController>();
+        XZRotController = XZRot.GetComponent<SimpleTouchController>();
+        YNavController = YNav.GetComponent<SimpleTouchController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 XZNavVec = XZNavController.GetTouchPosition;
+        Vector2 XZRotVec = XZRotController.GetTouchPosition;
+        Vector2 YNavVec = YNavController.GetTouchPosition;
+        if (isBuildMode)
+        {
+            if (isBuildingObj)
+            {
+                if (curObjType == "Air")
+                {
+                    currentObj.transform.Translate(new Vector3(XZNavVec.x, YNavVec.x, XZNavVec.y) * UIControlRate);
+                    currentObj.transform.Rotate(new Vector3(XZRotVec.x, YNavVec.x, XZRotVec.y));
+                }
+                else if (curObjType == "Ground")
+                {
+                    currentObj.transform.Translate(new Vector3(XZNavVec.x, 0, XZNavVec.y) * UIControlRate);
+                    currentObj.transform.Rotate(Vector3.up * XZRotVec.x);
+                }
+            }
+            
+        }
+    }
+
+    public void BuildModeBtnClick()
+    {
+        isBuildMode = true;
+        BuildModeBtn.SetActive(false);
+        showObjButtons();
+        ExitBuildModeBtn.SetActive(true);
+    }
+    
+    public void CarBtnClick()
+    {
+        isBuildingObj = true;
+        CarObj.SetActive(true);
+        currentObj = CarObj;
+        curObjType = "Ground";
+        toggleObjButtons("CAR");
+    }
+
+    public void HouseBtnClick()
+    {
+        isBuildingObj = true;
+        HouseObj.SetActive(true);
+        currentObj = HouseObj;
+        curObjType = "Ground";
+        toggleObjButtons("HOUSE");
+    }
+
+    public void AirplaneBtnClick()
+    {
+        isBuildingObj = true;
+        AirplaneObj.SetActive(true);
+        currentObj = AirplaneObj;
+        curObjType = "Air";
+        toggleObjButtons("AIRPLANE");
+    }
+
+    public void HotairballoonBtnClick()
+    {
+        isBuildingObj = true;
+        HotairballoonObj.SetActive(true);
+        currentObj = HotairballoonObj;
+        curObjType = "Air";
+        toggleObjButtons("HOTAIRBALLOON");
+    }
+
+    void toggleObjButtons(string objName)
+    {
+        bool[] status = new bool[4] {false, false, false, false};
+        switch (objName)
+        {
+            case "CAR":
+                status[0] = true;
+                break;
+            case "HOUSE":
+                status[1] = true;
+                break;
+            case "AIRPLANE":
+                status[2] = true;
+                break;
+            case "HOTAIRBALLOON":
+                status[3] = true;
+                break;
+            default:
+                break;
+        }
+        CarBtn.SetActive(status[0]);
+        HouseBtn.SetActive(status[1]);
+        AirplaneBtn.SetActive(status[2]);
+        HotairballoonBtn.SetActive(status[3]);
+        SaveCurrentObjBtn.SetActive(true);
+    }
+
+    void showObjButtons()
+    {
+        CarBtn.SetActive(true);
+        HouseBtn.SetActive(true);
+        AirplaneBtn.SetActive(true);
+        HotairballoonBtn.SetActive(true);
+    }
+
+    void hideObjButtons()
+    {
+        CarBtn.SetActive(false);
+        HouseBtn.SetActive(false);
+        AirplaneBtn.SetActive(false);
+        HotairballoonBtn.SetActive(false);
+    }
+
+    public void SaveCurrentObjClick()
+    {
+        isBuildingObj = false;
+        currentObj = null;
+        curObjType = "NULL";
+        showObjButtons();
+        SaveCurrentObjBtn.SetActive(false);
+    }
+
+    public void ExitBuildModeBtnClick()
+    {
+        isBuildMode = false;
+        BuildModeBtn.SetActive(true);
+        hideObjButtons();
+        ExitBuildModeBtn.SetActive(false);
+    }
+
+}
